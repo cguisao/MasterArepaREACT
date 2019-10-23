9b908c1006e7cb60c7f75207a5f11c37d2b972ee
Carlos Guisao <cguisao@fau.edu>
2019-10-23 00:10:14 -0400

    Extracted the User List in the Nav

--- Master Arepa/ClientApp/src/components_Admin/AdminNavbar.js
+++ Master Arepa/ClientApp/src/components_Admin/AdminNavbar.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import NavMenu from './NavMenu';
 
 function AdminNavbar() {
     return (<React.Fragment>
         <nav className="navbar navbar-expand-lg  fixed-top navbar-custom sticky-dark">
             <div className="container">
-                <Link className="navbar-brand logo" to="/">
-                    <img src="images/logo.jpg" alt="" height="95" />
-                </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <i className="mdi mdi-menu"></i>
-                </button>
+                
+                <UserListNav />
 
-                <div className="collapse navbar-collapse">
-                    <div className="navbar-nav ml-auto">
-                        <ul className="navbar-nav ml-auto navbar-center" id="mySidenav">
-
-                            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-                        </ul>
-                    </div>
-                </div>
                 <NavMenu />
             </div>
         </nav>
--- Master Arepa/ClientApp/src/components_Admin/VisitorNavbar.js
+++ Master Arepa/ClientApp/src/components_Admin/VisitorNavbar.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import NavMenu from "./NavMenu";
+import UserListNav from './UserListNav';
 
 function VisitorNavbar() {
 
     return (<React.Fragment>
         <nav className="navbar navbar-expand-lg  fixed-top navbar-custom sticky-dark">
             <div className="container">
-                <Link className="navbar-brand logo" to="/">
-                    <img src="images/logo.jpg" alt="" height="95" />
-                </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <i className="mdi mdi-menu"></i>
-                </button>
-
-                <div className="collapse navbar-collapse">
-                    <div className="navbar-nav ml-auto">
-                        <ul className="navbar-nav ml-auto navbar-center" id="mySidenav">
-
-                            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-                        </ul>
-                    </div>
-                </div>
+                
+                <UserListNav />
 
                 <NavMenu />
 
