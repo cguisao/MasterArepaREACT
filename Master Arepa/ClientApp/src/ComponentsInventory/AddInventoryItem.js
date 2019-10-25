b1a20e890ad5523ab76747c064c40c5472b6eb61
Carlos Guisao <cguisao@fau.edu>
2019-10-24 20:48:49 -0400

    Added authentication so that only admins can see this page.

--- Master Arepa/ClientApp/src/ComponentsInventory/AddInventoryItem.js
+++ Master Arepa/ClientApp/src/ComponentsInventory/AddInventoryItem.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Row, Col, Form, Input, Label, Button } from 'reactstrap';
+import { useAuth0 } from "../react-auth0-wrapper";
+import NotAuthenticated from "../components_Admin/NotAuthenticated";
+import Preloader from "../components/Preloader";
 
 function handleSubmit(event) {
 
@@ -24,7 +27,6 @@ function handleSubmit(event) {
             {
                 alert(data.error);
             }
-            //console.log(data.response);
         }
         else{
             // Show that there is an error on the server
@@ -36,46 +38,64 @@ function handleSubmit(event) {
 
 }
 
-function AddItem(){
-    
+function AdminInventory(){
     return (
         <section className="section bg-light" id="contact">
-        <br />
-        <br />
-        <br />
-      <div className="container">
-          <div className="row">
-              <Col lg="12">
-                  <div className="title-heading mb-5">
-                      <h2 className="text-dark mb-1 font-weight-light text-uppercase">Add Inventory Item</h2>
-                  </div>
-              </Col>
-          </div>
-          <Row>
-              <Col lg="12">
-                  <div className="contact-box p-5">
-                      <Row>
-                          <Col lg="8" md="6">
-                              <div className="custom-form p-3">
-                              <div id="message"></div>
-                                <Form onSubmit={handleSubmit}>
-                                    <Col sx="3">
-                                        <Label htmlFor="item">Item</Label>
-                                        <Input type="text" name="Item" id="Item" required="true" placeholder="New Item" />
-                                    </Col>
-                                    <Col>
-                                        <Button color="primary">Submit</Button>
-                                    </Col>
-                                </Form>
-                              </div>
-                          </Col>
-                      </Row>
-                  </div>
-              </Col>
-          </Row>
-      </div>
-  </section>
+            <br />
+            <br />
+            <br />
+            <div className="container">
+                <div className="row">
+                    <Col lg="12">
+                        <div className="title-heading mb-5">
+                            <h2 className="text-dark mb-1 font-weight-light text-uppercase">Add Inventory Item</h2>
+                        </div>
+                    </Col>
+                </div>
+                <Row>
+                    <Col lg="12">
+                        <div className="contact-box p-5">
+                            <Row>
+                                <Col lg="8" md="6">
+                                    <div className="custom-form p-3">
+                                    <div id="message"></div>
+                                        <Form onSubmit={handleSubmit}>
+                                            <Col sx="3">
+                                                <Label htmlFor="item">Item</Label>
+                                                <Input type="text" name="Item" id="Item" required="true" placeholder="New Item" />
+                                            </Col>
+                                            <Col>
+                                                <Button color="primary">Submit</Button>
+                                            </Col>
+                                        </Form>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        </section>
     );
+}
+
+function AddItem(){
+    
+    const { loading, isAuthenticated, user } = useAuth0();
+
+    if (loading) {
+        return <Preloader />;
+    }
+
+    var role = user[Object.keys(user)[0]];
+
+    if(isAuthenticated && role == "Admin"){
+        return <AdminInventory />;
+    }
+    else{
+        return <NotAuthenticated />;
+    }
+    
 }
 
 export default AddItem;
