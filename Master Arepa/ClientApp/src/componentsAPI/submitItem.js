b5d96b6dc2fa90a42fc693c074f7218acaca781d
Carlos Guisao <cguisao@fau.edu>
2019-11-12 19:28:07 -0500

    Added logic to add item type dynamically

---
+++ Master Arepa/ClientApp/src/componentsAPI/submitItem.js
@@ -0,0 +1,34 @@
+const handleSubmit = url => event => {
+    console.log(url);
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch(url, {
+        method: 'POST',
+        body: data
+    }).then(function(response){
+        return response.json();
+    }).then(function(data){
+        if(data.response != undefined){
+            // Show that nothing went wrong
+            // Show that Item is already in the database
+            if(data.response == "Success"){
+                alert("Item added successfully!");
+                window.location.reload();
+            }
+            // Show that the Item has successfully been added then reload the page
+            else if(data.response == "Error")
+            {
+                alert(data.error);
+            }
+        }
+        else{
+            // Show that there is an error on the server
+            console.log("Error on server info: \n" + "data.response" + data.ClassName + "\n" + "data.response" + data.response);
+        }
+    }).catch(function(err){
+        console.log("Server completely down info: \n" + err);
+    })
+}
+
+export default handleSubmit;
---
+++ Master Arepa/Migrations/20191110205807_Arepa.V1.6.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Master_Arepa.Migrations
+{
+    public partial class ArepaV16 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InventoryItemType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryItemType", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InventoryItemType");
+        }
+    }
+}
